688f1950a6e930b0e5a048b0218ca020fc6ef91e
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-09-25 12:27:47 -0400

    q3 done

--- src/com/Manbir/arraysandstrings/exercise1/Q3.java
+++ src/com/Manbir/arraysandstrings/exercise1/Q3.java
@@ -3,20 +3,22 @@ package com.Manbir.arraysandstrings.exercise1;
 public class Q3 {
 //    Write a function that when given a char array replaces all ‘b’s with ‘9’s
 
-    public static void main(String[] args) {
-        char[] arr = new char[] {'a','B','e','d','e','b','r','b'};
-        replaceB(arr);
+    public static void main(String args[]) {
+        char[] arr = new char[] {'a','B','e','d','e','b','r','b','z','e'};
+        replaceWith9(arr);
     }
 
-    public static void replaceB(char[] arr1) {
-            if ((arr1 == null) || (arr1.length == 0)){
-                System.out.println("empty array");
-            }
-        for (int i = 0; i < arr1.length; i++) {
-            if (Character.toLowerCase(arr1[i]) == 'b') {
-                arr1[i] = '9';
+    public static void replaceWith9(char[] arr) {
+        if ((arr == null) || (arr.length == 0)){
+            System.out.println("empty array");
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (Character.toLowerCase(arr[i]) == 'b') {
+                arr[i] = '9';
             }
         }
-        System.out.println(arr1);
+
+        System.out.println("the array after replacing is: " + new String(arr));
     }
 }
